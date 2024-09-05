7a280d96dc8b555b685b1feeee6b5a28d0bb9a57
Thao Do <thaobean1701@gmail.com>
2024-09-05 14:34:43 +0700

    authentication UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components";
 
 export default function Home() {
-  return <Button variant='slack'>Click me</Button>;
+  return <AuthScreen />;
 }
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  Input,
+  Separator,
+} from "@/components/ui";
+import React, { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { SignInFlow } from "../types";
+
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignInCard: React.FC<SignInCardProps> = ({ setState }) => {
+  const [value, setValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChangeValue = (name: string, _value: string) => {
+    setValue({
+      ...value,
+      [name]: _value,
+    });
+  };
+
+  return (
+    <Card className='w-full h-full p-8'>
+      <CardHeader className='px-0 pt-0'>
+        <CardTitle>Login to continue </CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className='space-y-5 px-0 pb-0'>
+        <form className='space-y-2.5'>
+          <Input
+            value={value.email}
+            onChange={(e) => handleChangeValue("email", e.target.value)}
+            type='email'
+            placeholder='Email'
+            required
+          />
+          <Input
+            value={value.password}
+            onChange={(e) => handleChangeValue("password", e.target.value)}
+            type='password'
+            placeholder='Password'
+            required
+          />
+          <Button type='submit' className='w-full' size='lg'>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className='flex flex-col gap-y-2.5'>
+          <Button
+            onClick={() => {}}
+            variant='outline'
+            size='lg'
+            className='w-full relative'
+          >
+            <FcGoogle className='size-5 absolute top-3 left-2.5' />
+            Continue with google
+          </Button>
+
+          <Button
+            onClick={() => {}}
+            variant='outline'
+            size='lg'
+            className='w-full relative'
+          >
+            <FaGithub className='size-5 absolute top-3 left-2.5' />
+            Continue with github
+          </Button>
+        </div>
+        <p className='text-xs text-muted-foreground'>
+          Already have an account?{" "}
+          <span
+            className='text-sky-700  hover:underline cursor-pointer'
+            onClick={() => setState("signUp")}
+          >
+            Sign up
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
