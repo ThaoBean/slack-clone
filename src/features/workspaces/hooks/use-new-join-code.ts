8f6495d7546e26de7af9097f272389ea9d61fa19
Thao Do <thaobean1701@gmail.com>
2024-10-02 15:45:39 +0700

    feat: workspace invite modal

---
+++ src/features/workspaces/hooks/use-new-join-code.ts
@@ -0,0 +1,46 @@
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useCallback, useMemo, useState } from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = { workspaceId: Id<"workspaces"> };
+type ResponseType = Id<"workspaces"> | null;
+
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (err: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+export const useNewJoinCode = () => {
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+
+  const mutation = useMutation(api.workspaces.newJoinCode);
+
+  const mutate = useCallback(
+    async (values: RequestType, options?: Options) => {
+      try {
+        setStatus("pending");
+
+        const response = await mutation(values);
+        options?.onSuccess?.(response);
+        return response;
+      } catch (err) {
+        setStatus("error");
+        options?.onError?.(err as Error);
+        if (options?.throwError) throw err;
+      } finally {
+        setStatus("settled");
+        options?.onSettled?.();
+      }
+    },
+    [mutation],
+  );
+
+  return { mutate, isPending };
+};
