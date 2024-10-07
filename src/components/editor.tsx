a0746930a303ca195903b13c94cdd7ea972f437a
Thao Do <thaobean1701@gmail.com>
2024-10-07 13:40:06 +0700

    feat: editor component

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { MutableRefObject, useEffect, useRef } from "react";
+import {
+  MutableRefObject,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import Quill, { type QuillOptions } from "quill";
 import { Delta, Op } from "quill/core";
 import { PiTextAa } from "react-icons/pi";
@@ -10,14 +16,15 @@ import { Hint } from "./hint";
 import { Button } from "./ui";
 
 import "quill/dist/quill.snow.css";
+import { cn } from "@/lib/utils";
 type EditorValue = {
   image: File | null;
   body: string;
 };
 
 interface EditorProps {
-  // onSubmit: ({ image, body }: EditorValue) => void;
-  // onCancel: () => void;
+  onSubmit: ({ image, body }: EditorValue) => void;
+  onCancel?: () => void;
   placeholder?: string;
   defaultValue?: Delta | Op[];
   disabled?: boolean;
@@ -26,17 +33,30 @@ interface EditorProps {
 }
 
 const Editor = ({
-  // onCancel,
-  // onSubmit,
+  onCancel,
+  onSubmit,
   placeholder = "Write something...",
   defaultValue = [],
   disabled = false,
   innerRef,
   variant = "create",
 }: EditorProps) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [text, setText] = useState("");
+  const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
-  // const submitRef = useRef(onSubmit);
+  const submitRef = useRef(onSubmit);
+  const placeholderRef = useRef(placeholder);
+  const quillRef = useRef<Quill | null>(null);
+  const defaultValueRef = useRef(defaultValue);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const disableRef = useRef(disabled);
+
+  useLayoutEffect(() => {
+    submitRef.current = onSubmit;
+    placeholderRef.current = placeholder;
+    defaultValueRef.current = defaultValue;
+    disableRef.current = disabled;
+  });
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -48,35 +68,93 @@ const Editor = ({
 
     const options: QuillOptions = {
       theme: "snow",
+      placeholder: placeholderRef.current,
+      modules: {
+        toolbar: [
+          ["bold", "italic", "strike"],
+          ["link"],
+          [{ list: "ordered" }, { list: "bullet" }],
+        ],
+        keyboard: {
+          bindings: {
+            enter: {
+              key: "Enter",
+              handler: () => {
+                //TODO: submit form
+                return;
+              },
+            },
+            shift_enter: {
+              key: "Enter",
+              shiftKey: true,
+              handler: () => {
+                quill.insertText(quill.getSelection()?.index || 0, "\n");
+              },
+            },
+          },
+        },
+      },
     };
 
-    new Quill(editorContainer, options);
+    const quill = new Quill(editorContainer, options);
+    quillRef.current = quill;
+    quillRef.current.focus();
+    if (innerRef) {
+      innerRef.current = quill;
+    }
+
+    quill.setContents(defaultValueRef.current);
+    setText(quill.getText());
+
+    quill.on(Quill.events.TEXT_CHANGE, () => {
+      setText(quill.getText());
+    });
 
     return () => {
+      quill.off(Quill.events.TEXT_CHANGE);
       if (container) {
         container.innerHTML = "";
       }
+      if (quillRef.current) {
+        quillRef.current = null;
+      }
+      if (innerRef) {
+        innerRef.current = null;
+      }
     };
-  }, []);
+  }, [innerRef]);
+
+  const toggleToolbar = () => {
+    setIsToolbarVisible((current) => !current);
+    const toolbarElement = containerRef.current?.querySelector(".ql-toolbar");
+
+    if (toolbarElement) {
+      toolbarElement.classList.toggle("hidden");
+    }
+  };
+
+  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className='flex flex-col'>
       <div className='flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white'>
         <div ref={containerRef} className='h-full ql-custom' />
         <div className='flex px-2 pb-2 z-[5]'>
-          <Hint label='Hide formatting'>
+          <Hint
+            label={isToolbarVisible ? "Hide formatting" : "Show formatting"}
+          >
             <Button
-              disabled={false}
+              disabled={disabled}
               size='iconSm'
               variant='ghost'
-              onClick={() => {}}
+              onClick={toggleToolbar}
             >
               <PiTextAa className='size-4' />
             </Button>
           </Hint>
           <Hint label='Emoji'>
             <Button
-              disabled={false}
+              disabled={disabled}
               size='iconSm'
               variant='ghost'
               onClick={() => {}}
@@ -88,7 +166,7 @@ const Editor = ({
           {variant === "create" && (
             <Hint label='Image'>
               <Button
-                disabled={false}
+                disabled={disabled}
                 size='iconSm'
                 variant='ghost'
                 onClick={() => {}}
@@ -104,14 +182,14 @@ const Editor = ({
                 variant='outline'
                 size='sm'
                 onClick={() => {}}
-                disabled={false}
+                disabled={disabled}
               >
                 Cancel
               </Button>
               <Button
                 size='sm'
                 onClick={() => {}}
-                disabled={false}
+                disabled={disabled || isEmpty}
                 className='bg-[#007a5a] hover:bg-[#007a54]/80 text-white'
               >
                 Save
@@ -120,10 +198,15 @@ const Editor = ({
           )}
           {variant === "create" && (
             <Button
-              disabled={false}
+              disabled={disabled || isEmpty}
               onClick={() => {}}
               size='sm'
-              className='ml-auto bg-[#007a5a] hover:bg-[#007a54]/80 text-white'
+              className={cn(
+                "ml-auto",
+                isEmpty
+                  ? "bg-[#fff] hover:bg-[#fff] text-muted-foreground"
+                  : "bg-[#007a5a] hover:bg-[#007a54]/80 text-white",
+              )}
             >
               <MdSend className='size-4' />
             </Button>
